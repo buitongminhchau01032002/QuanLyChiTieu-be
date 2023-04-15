2e977e6cbb471c237fe91b0720962f7dd09b2f05
Minh Chau <buitongminhchau01032002@gmail.com>
2023-04-15 10:16:39 +0700

    filter transaction by month

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -4,10 +4,21 @@ import Wallet from '../models/Wallet.js';
 
 // [GET] /transactions/of-user
 export const getTransactionsOfUser = async (req, res, next) => {
-    const month = req.query.month;
+    const searchMonth = req.query.month;
     const user = req.user;
+    const objFilter = { user: user._id };
+    if (searchMonth) {
+        const [month, year] = searchMonth.split('-').map(Number);
+
+        const startOfMonth = new Date(year, month - 1, 1); // set start date to first day of month
+        const endOfMonth = new Date(year, month, 0, 23, 59, 59, 999); // set end date to last day of month
+        objFilter.createdAt = {
+            $gte: startOfMonth,
+            $lt: endOfMonth,
+        };
+    }
     try {
-        const transactions = await Transaction.find({ user: user._id })
+        const transactions = await Transaction.find(objFilter)
             .populate('wallet')
             .populate('category')
             .populate({ path: 'user', select: '-password' });
